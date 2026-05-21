443ba5bb38361e6d43b4df6ce78fcea668fa1a31
Artur <artur.kizmich.2007@gmail.com>
2026-05-21 14:57:40 +0300

    v2.0

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 import time
 
 app = FastAPI()
+time_requests_count = 0
 
 @app.get("/time")
 def get_time():
+    global time_requests_count
+    time_requests_count += 1
     return {"time": int(time.time())}
+
+@app.get("/metrics")
+def get_metrics():
+    return {"count": time_requests_count}
